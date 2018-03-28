e3e1e18867dc817215693b201cd6f8f90f14265c
Dennis Huh <denjhuh@gmail.com>
2018-03-27 23:53:37 -0500

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
@@ -28,6 +29,24 @@ app.get('/todos', (req, res) => {
     res.status(400).send(e);
   })
 });
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  Todo.findById(id).then((todo) => {
+    console.log(todo)
+    if (todo) {
+      res.send(todo);
+    } else {
+      res.send();
+    }
+    
+  }, (e) => {
+    res.status(400).send(e)
+  })
+})
 
 app.listen(5000, () => {
   console.log('Started on port 5000');
